a3f4b302c5eab5bb42595ce7da9c2056fd2a007d
anbudhanush <42831942+anbudhanush@users.noreply.github.com>
2018-11-19 15:53:16 +0000

    Add files via upload

--- sampleobjects.php
+++ sampleobjects.php
@@ -4,16 +4,13 @@ $file = fopen("sampleobjects.txt", "r");
 
 $raw_text = fread($file,filesize("sampleobjects.txt"));
 
-//foreach( $array as $key => $value ){
-//    echo $key."\t=>\t".$value."\n";
-	
 	
 foreach ($raw_text as $item) {
    $json =	json_encode($structure, true); 
 }
 
 print_r(htmlspecialchars($json));
-echo "<pre>";
+var_dump ($json);
 $converted_file = fopen("jsonconverted.txt", "w");
 fwrite($converted_file, $json);
 
