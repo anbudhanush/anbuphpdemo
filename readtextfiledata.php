6d5c5319bf3dc9876b28e55bedcd9f5aa4a92c68
anbudhanush <42831942+anbudhanush@users.noreply.github.com>
2018-11-20 09:17:03 +0000

    Add files via upload

--- readtextfiledata.php
+++ readtextfiledata.php
@@ -4,13 +4,13 @@ $file = fopen("thisistextfile.txt", "r");
 
 $raw_text = fread($file,filesize("thisistextfile.txt"));
 
-echo $raw_text	
+//echo $raw_text	
 //foreach ($raw_text as $item) {
 //}
 
 //print_r(htmlspecialchars($json));
 //var_dump ($item);
-//$converted_file = fopen("thisistextnewfile.txt", "w");
-//fwrite($converted_file, $json);
+$converted_file = fopen("thisistextnewfile.txt", "w");
+fwrite($converted_file, $raw_text);
 
 ?>
